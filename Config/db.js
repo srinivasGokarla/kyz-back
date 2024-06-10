4db68bfa7e81505982d10562de5ab4c2fd9a737e
srinivasGokarla <srinivasgokarla@gmail.com>
2024-06-11 01:30:59 +0530

    changes

--- Config/db.js
+++ Config/db.js
@@ -1,11 +1,15 @@
 const mongoose = require("mongoose");
 
-const connectDB = async (req, res) => {
+const connectDB = async () => {
   try {
-    const connection = await mongoose.connect(process.env.MONGO_URI);
+    const connection = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("Database Connected");
   } catch (error) {
-    console.log(error);
+    console.log("Error connecting to database:", error);
+    process.exit(1); // Exit process with failure
   }
 };
 
--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,12 +1,34 @@
+// const jwt = require("jsonwebtoken");
+// const userModel = require("../Model/UserModel");
+
+// const authMiddleware = async (req, res, next) => {
+//   const token = req.header("Authorization").replace("Bearer ", "");
+
+//   if (!token) {
+//     return res.status(401).send({ message: "No token, authorization denied" });
+//   }
+
+//   try {
+//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+//     req.user = await userModel.findById(decoded.userId).select("-password");
+//     next();
+//   } catch (err) {
+//     res.status(401).send({ message: "Token is not valid" });
+//   }
+// };
+
+// module.exports = authMiddleware;
+
 const jwt = require("jsonwebtoken");
 const userModel = require("../Model/UserModel");
 
 const authMiddleware = async (req, res, next) => {
-  const token = req.header("Authorization").replace("Bearer ", "");
-
-  if (!token) {
+  const authHeader = req.header("Authorization");
+  if (!authHeader) {
     return res.status(401).send({ message: "No token, authorization denied" });
   }
+
+  const token = authHeader.replace("Bearer ", "");
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
