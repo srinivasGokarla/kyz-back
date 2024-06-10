7dc99e5584fc3b1408ef97885d097983d05933ca
srinivasGokarla <srinivasgokarla@gmail.com>
2024-06-11 01:40:15 +0530

    need

--- Config/db.js
+++ Config/db.js
@@ -2,13 +2,13 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const connection = await mongoose.connect(process.env.MONGO_URI, {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
     console.log("Database Connected");
   } catch (error) {
-    console.log("Error connecting to database:", error);
+    console.error("Error connecting to database:", error);
     process.exit(1); // Exit process with failure
   }
 };
--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const cors = require("cors");
-const dotenv = require("dotenv")
+const dotenv = require("dotenv");
+
+// Load environment variables from .env file
+dotenv.config();
+
 const connectDB = require("./Config/db");
 const userRouter = require("./Routes/UserRouter");
 const ProductRouter = require("./Routes/ProductRoute");
-dotenv.config(); 
+
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -12,8 +16,10 @@ app.use(cors());
 
 app.use("/api/user", userRouter);
 app.use("/api/product", ProductRouter);
+
 const PORT = process.env.PORT || 5550;
 connectDB();
+
 app.listen(PORT, () => {
   console.log(`Server Started at port No http://localhost:${PORT}`);
 });
