1a302c90d7e7e23cd37daad0f24169d592cb9cf2
srinivasGokarla <srinivasgokarla@gmail.com>
2024-06-11 07:54:34 +0530

    testing

--- Config/db.js
+++ Config/db.js
@@ -2,14 +2,13 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    const connection = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
     console.log("Database Connected");
   } catch (error) {
     console.error("Error connecting to database:", error);
-    process.exit(1); // Exit process with failure
   }
 };
 
